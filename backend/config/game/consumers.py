ac8cd52e362f29418f7a2a6a6e17dab51379caea
Abdullah Suhail <abdullahcodes1812@gmail.com>
2026-09-16 17:35:39 +0530

    Entering clue phase success

--- backend/config/game/consumers.py
+++ backend/config/game/consumers.py
@@ -42,15 +42,77 @@ class GameConsumer(WebsocketConsumer):
         print("GROUP:", self.room_group_name)
 
     def receive(self, text_data):
+
         print("WE HAVE ENTERED RECEIVE")
 
         data = json.loads(text_data)
 
         print("ENTER THIS AFTER DONE ", text_data)
 
+        if data.get("type") == "next_turn":
+
+            print("CONSUMER OF NEXT TURN")
+
+            game_state = GameState.objects.get(
+            room=self.player.room
+           )
+
+            # Make sure only the current player can press NEXT
+            if game_state.current_player != self.player:
+                return
+
+            players = list(
+                self.player.room.players.order_by("joined_at")
+            )
+
+            current_index = players.index(
+                game_state.current_player
+            )
+
+            # Check if this was the last player
+            if current_index == len(players) - 1:
+
+                game_state.phase = GameState.Phase.VOTING
+                game_state.current_player = None
+                game_state.save()
+
+                async_to_sync(self.channel_layer.group_send)(
+                    self.room_group_name,
+                    {
+                        "type": "voting_started"
+                    }
+                )
+
+            else:
+                    
+                    next_player = players[current_index + 1]
+                    game_state.current_player = next_player
+                    game_state.save()
+
+                    async_to_sync(self.channel_layer.group_send)(
+                        self.room_group_name,
+                        {
+                            "type": "turn_changed",
+                            "player_id": str(next_player.player_id),
+                            "nickname": next_player.nickname
+                        }
+                    )
+
+        if data.get("type") == "clue_connected":
+
+            game_state = GameState.objects.get(
+                room=self.player.room
+            )
+
+            self.send(text_data=json.dumps({
+                "type": "current_player",
+                "player_id": str(game_state.current_player.player_id),
+                "nickname": game_state.current_player.nickname
+            }))
+
         if data.get("type") == "startClue":
 
-            # print("ENTER if condition")
+           
             if not self.player.is_host:
                 return
 
@@ -62,11 +124,15 @@ class GameConsumer(WebsocketConsumer):
                 room=self.player.room
             )
 
+            print("The first player is ",first_player)
+
             game_state.phase = GameState.Phase.CLUE
             game_state.current_player = first_player
             game_state.save()
 
             firstplayer=players[0]
+
+            print("CONSUMER STARTCLUE PHASE")
 
             async_to_sync(self.channel_layer.group_send)(
                 self.room_group_name,
@@ -80,8 +146,10 @@ class GameConsumer(WebsocketConsumer):
 
     def clue_started(self, event):
 
-        print("CLUE_STATED HANDLER CALLED")
+        print("CLUE_STARTED HANDLER CALLED")
 
         self.send(text_data=json.dumps({
-            "type": "clue_started"
+            "type": "clue_started",
+            "player_id": event["player_id"],
+            "nickname": event["nickname"]
         }))
